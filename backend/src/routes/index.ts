6478ba6d0e2d02ca7dde29791a204ab8d0b5af6a
Sebastian Liius <105113073+seblii@users.noreply.github.com>
2023-12-07 17:50:35 +0200

    Added swagger controller

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express';
 import listNames from './list-names';
+import swagger from './swagger';
+
 
 export const initRoutes = (app: Express) => {
     listNames(app);
+    swagger(app);
 }
 
 export default initRoutes;
