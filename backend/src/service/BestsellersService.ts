7af95b1a0497075dedbec02f5cfaaad566985e15
Sebastian Liius <105113073+seblii@users.noreply.github.com>
2023-12-08 20:53:30 +0200

    Implemented cache for BestsellersService

--- backend/src/service/BestsellersService.ts
+++ backend/src/service/BestsellersService.ts
@@ -21,6 +21,22 @@ type ReviewReference = {
     summary: string | undefined;
 };
 
+type FunctionType = (...args: any[]) => any;
+
+// TODO: Fix the cache
+function cacheFunction<T extends FunctionType>(fn: T): T {
+    let cache: any = {};
+    return function (...args: any[]) {
+        const key: string = JSON.stringify(args);
+        if (!cache[key]) {
+            cache[key] = fn(...args);
+        } else {
+            console.log("Response from cache.")
+        }
+        return cache[key];
+    } as T;
+}
+
 class BestellersService {
     apiKey: string = "";
 
@@ -28,11 +44,16 @@ class BestellersService {
         if (!apiKey) {
             throw new Error("NYT_API_KEY must be given!");
         }
+
         this.apiKey = apiKey;
     }
 
+    _getListsNamesFormat = cacheFunction(async () => {
+        return await DefaultService.getListsNamesFormat(this.apiKey);
+    })
+
     async getBooklists(): Promise<BookList[]> {
-        const rawResponse = await DefaultService.getListsNamesFormat(this.apiKey);
+        const rawResponse = await this._getListsNamesFormat();
         const lists = rawResponse.results?.map(
             ({ list_name, display_name, list_name_encoded }) => {
                 return { list_name, display_name, list_name_encoded };
@@ -42,14 +63,14 @@ class BestellersService {
         return lists ? lists : [];
     }
 
+    _getListsDateListJson = cacheFunction(async (encodedListName: string) => {
+        return await DefaultService.getListsDateListJson("current", encodedListName, this.apiKey);
+    })
+
     async getBestsellersOfCategory(
         encodedListName: string
     ): Promise<Book[] | undefined> {
-        const rawResponse = await DefaultService.getListsDateListJson(
-            "current",
-            encodedListName as string,
-            this.apiKey
-        );
+        const rawResponse = await this._getListsDateListJson(encodedListName);
 
         const books = rawResponse.results?.books?.map(
             ({ rank, title, author, primary_isbn10, primary_isbn13, book_image }) => {
@@ -67,11 +88,12 @@ class BestellersService {
         return books ? books : [];
     }
 
+    _getReviewsFormat = cacheFunction(async (isbn: number) => {
+        return await DefaultService.getReviewsFormat(this.apiKey, isbn);
+    })
+
     async getBookReviewReferences(isbn: number): Promise<ReviewReference[]> {
-        const rawResponse = await DefaultService.getReviewsFormat(
-            this.apiKey,
-            isbn
-        );
+        const rawResponse = await this._getReviewsFormat(isbn);
 
         const reviewRefs = rawResponse.results?.map(({ url, byline, summary }) => {
             return {
