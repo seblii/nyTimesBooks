b4db37ff31d02f0d04655aced39a3ebd54303311
Sebastian Liius <105113073+seblii@users.noreply.github.com>
2023-12-13 20:44:06 +0200

    Fixed vite problem, refactoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import BookCategoryList from "./components/BookCategoryList";
+import BookList from "./components/BookList";
+import { ReviewList } from "./components/Review";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<BookCategoryList />} />
+        <Route path="/list/:encodedListName" element={<BookList />} />
+        <Route path="/review/:isbn13" element={<ReviewList />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
